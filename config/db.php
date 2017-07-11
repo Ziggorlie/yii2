6a53823867596527ecdb3a59c5bbf4b4d03002fc
Alexey <onphp@yandex.ru>
2017-07-11 18:58:44 +0300

    2

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=p444578_yii',
+    'username' => 'p444578_ziggor',
+    'password' => 'folk2736',
+    'charset' => 'utf8',
+];
